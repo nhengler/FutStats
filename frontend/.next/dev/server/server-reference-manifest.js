6016e8d4f7adbc04fd3e4c0cef481425b9936805
unknown <niko_las1934@hotmail.com>
2026-01-30 04:40:30 -0300

    remove .next of push

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Tkov/EFGgdtL84T8tfqY8TSUUHpX5ms+h85nqacRnrc=\"\n}"
--- frontend/.next/dev/static/chunks/app_layout_tsx_3873bb71._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/styles_globals_9fb35f7d.css",
-  "static/chunks/node_modules_962dceb9._.js",
-  "static/chunks/components_ec8bf0a2._.js"
-],
-    source: "dynamic"
-});
